fb1aa68d29347ea8407895a080eccdbdb59fcaa4
TetyLike3 <t.kerbalwhoflies@gmail.com>
2022-10-19 16:45:33 +0100

    Renamed Form1 to MainForm

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace TetyCompliantCalculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
